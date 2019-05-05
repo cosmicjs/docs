9675b497d25e1edab172d1001ebcec1893939ee5
Ben Hong <bencodezen@gmail.com>
2019-05-05 18:54:06 -0400

    :wrench: Configure Google Fonts

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  siteTitle: 'Cosmic JS',
   themeConfig: {
+    logo: '/cosmicjs.svg',
     repo: 'cosmicjs/docs',
     docsDir: 'src',
     editLinks: true,
     editLinkText: 'Edit this page',
     lastUpdated: 'Last Updated'
-  }
+  },
+  head: [
+    ['link', {
+      href: "//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600",
+      rel: "stylesheet",
+      type: "text/css"
+    }]
+  ]
 }
