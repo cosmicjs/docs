b0b47a6f4046201d2338d770a3b06a72992548fc
Ben Hong <bencodezen@gmail.com>
2019-05-05 14:28:24 -0400

    :wrench: Configure Edit This Page feature

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,9 @@
 module.exports = {
   themeConfig: {
+    repo: 'cosmicjs/docs',
+    docsDir: 'src',
+    editLinks: true,
+    editLinkText: 'Edit this page',
     lastUpdated: 'Last Updated'
   }
 }
